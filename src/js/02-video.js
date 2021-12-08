f50c114ad940ad1658ea267da2aa522bf84c3611
Maxim Samsonenko <samsonenko.max@gmail.com>
2021-12-08 19:50:07 +0200

    comments added

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,9 @@
 import throttle from 'lodash.throttle';
-
+//get access to iframe
 const iframe = document.querySelector('iframe');
+//create player instance
 const player = new Vimeo.Player(iframe);
-
+//when "play" btn pressed checks if local storage has time save for the play, and plays the video from saved point
 player.on('play', function () {
   const savedTime = localStorage.getItem('videoplayer-current-time');
 
@@ -10,14 +11,14 @@ player.on('play', function () {
     player.setCurrentTime(savedTime);
   }
 });
-
+//during play, save current video time to local storage
 player.on(
   'timeupdate',
   throttle(function ({ seconds }) {
     localStorage.setItem('videoplayer-current-time', seconds);
   }, 1000),
 );
-
+//sets local storage value to the value of chosen time play
 player.on('seeking', function ({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,32 @@
 import throttle from 'lodash.throttle';
-
+//storage key name
 const STORAGE_KEY = 'feedback-form-state';
-
+//an empty object for storing input fields values
 const userMessage = {};
+//get access to to form elelement
 const form = document.querySelector('.feedback-form');
-
+//handling form input values update
 form.addEventListener('input', throttle(handleUserInput, 500));
-
+//handling form submit
 form.addEventListener('submit', handleFormSubmit);
 populateForm();
-
+//on form submit, prevents default behaviour,
+//shows the saved object in console,
+//clears form input fields,
+//and clears local storage
 function handleFormSubmit(e) {
   e.preventDefault();
   console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   e.currentTarget.reset();
   localStorage.clear();
 }
-
+//on input value update, creates/updates key/value of the userMessage obj,
+//and saves data to local storage
 function handleUserInput(e) {
   userMessage[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(userMessage));
 }
-
+//on page load checks if local storage has data, and populates it to form fields if true
 function populateForm() {
   const formInputsData = localStorage.getItem(STORAGE_KEY);
 
